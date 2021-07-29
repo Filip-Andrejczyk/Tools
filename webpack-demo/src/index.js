4443966dd05b77541cd6ff44235e25d9a671d9e8
Filip-Andrejczyk <filip.andrejczyk@student.wat.edu.pl>
2021-07-29 13:02:15 +0200

    hello_webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 
-function component(){
+function component() {
     const element = document.createElement('div');
 
+    
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
     return element;
-}
-
-document.body.appendChild(component());
+  }
+  
+  document.body.appendChild(component());
